8189b61d70e53b22de4aa26daee6ade56012ea4c
SVengat03 <33342399+SVengat03@users.noreply.github.com>
2018-09-30 00:02:41 +0800

    Bugsy Fixies

--- quantumutils/__init__.py
+++ quantumutils/__init__.py
@@ -1,2 +1,7 @@
 name="quantum_utils"
-from . import paginator,simplepaginator,simpleutils
+from .paginator import *
+from .simplepaginator import *
+from .simpleutils import *
+
+if __name__=="__main__":
+  print(find("we","we are alweys number twenty"))
